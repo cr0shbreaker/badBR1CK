7a38c9b4c7f6d46d0e3475febc522e126ce325b0
Buypass07 <idkchigo@gmail.com>
2026-04-22 14:56:34 -0400

    Add unenrollDevice method and button in idk.vue

---
+++ idk.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="unenrollDevice">Unenroll Device</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    unenrollDevice() {
+      // Replace this with your custom implementation to execute the shell script
+      // Execute the unenroll_device.sh script
+      console.log("Unenrollment successful!");
+    }
+  }
+}
+</script>
